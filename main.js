8ff10420929fcc9687e963a0449f9294d6512677
tmdgus95 <8571053@naver.com>
2023-01-03 08:24:35 +0900

    JS: Navbar

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// 네브바 높이에 따른 투명도
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
