f63f78086ab77758129ca82235b5363e7c45e7b1
tmdgus95 <8571053@naver.com>
2023-01-03 09:13:06 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -28,6 +28,13 @@ homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
+// 홈 투명하게 만들기
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selectior) {
     const scrollTo = document.querySelector(selectior);
     scrollTo.scrollIntoView({ behavior: 'smooth' });
